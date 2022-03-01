fafcab7c33dfec8e95ea559ea44914ca3f804e47
samuel wangai <44896330+swangai7178@users.noreply.github.com>
2022-03-01 16:19:00 +0300

    updated

--- login_php/lib/main.dart
+++ login_php/lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
  import 'si.dart';
 void main() => runApp(MyApp());
+
  
 class MyApp extends StatelessWidget {
+  
  
   static const String _title = 'login';
  
@@ -29,9 +31,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
 
   @override
   Widget build(BuildContext context) {
-    MaterialApp(
-     debugShowCheckedModeBanner: false
-    );
+   
     return Padding(
         padding: const EdgeInsets.all(10),
         child: ListView(
@@ -111,3 +111,107 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   }
 }
 
+
+class SecondRoute extends StatefulWidget{
+  const SecondRoute({
+    Key? key,
+    this.color = const Color(0xFFFFE306),
+    this.child,
+  }) : super(key: key);
+
+  final Color color;
+  final Widget? child;
+
+  @override
+  State<SecondRoute> createState() => _SecondRoute();
+    }
+class _SecondRoute extends State<SecondRoute> {
+
+TextEditingController nameController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    //MaterialApp(
+    //  debugShowCheckedModeBanner: false
+    // );
+
+   
+    return Padding(
+        padding: const EdgeInsets.all(10),
+        child: ListView(
+          children: <Widget>[
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'mwangi.inc',
+                  style: TextStyle(
+                      color: Colors.blue,
+                      fontWeight: FontWeight.w500,
+                      fontSize: 30),
+                )),
+            Container(
+                alignment: Alignment.center,
+                padding: const EdgeInsets.all(10),
+                child: const Text(
+                  'Register',
+                  style: TextStyle(fontSize: 20),
+                )),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                controller: nameController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'User Name',
+                ),
+              ),
+            ),
+            Container(
+              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
+              child: TextField(
+                obscureText: true,
+                controller: passwordController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  labelText: 'Password',
+                ),
+              ),
+            ),
+            TextButton(
+              onPressed: () {
+                //forgot password screen
+              },
+              child: const Text('Forgot Password',),
+            ),
+            Container(
+                height: 50,
+                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
+                child: ElevatedButton(
+                  child: const Text('Login'),
+                  onPressed: () {
+                    print(nameController.text);
+                    print(passwordController.text);
+                  },
+                )
+            ),
+            Row(
+              children: <Widget>[
+                const Text('Does not have account?'),
+                TextButton(
+                  child: const Text(
+                    'Sign in',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                  onPressed: () {
+                   Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRoute()),);
+                  },
+                )
+              ],
+              mainAxisAlignment: MainAxisAlignment.center,
+            ),
+          ],
+        ));
+  }
+}
