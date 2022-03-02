3e4a27580416085fbd7834504b6c76b10d694422
samuel wangai <44896330+swangai7178@users.noreply.github.com>
2022-03-02 10:34:32 +0300

    new teme

--- login_php/lib/main.dart
+++ login_php/lib/main.dart
@@ -9,6 +9,8 @@ class MyApp extends StatelessWidget {
   
  
   static const String _title = 'login';
+
+  const MyApp({Key? key}) : super(key: key);
  
   @override
   Widget build(BuildContext context) {
@@ -23,93 +25,81 @@ class MyApp extends StatelessWidget {
 }
 
 class MyStatefulWidget extends StatefulWidget {
+  const MyStatefulWidget({Key? key}) : super(key: key);
+
   @override
   State<MyStatefulWidget> createState() => _MyStatefulWidgetState();
 }
 
 class _MyStatefulWidgetState extends State<MyStatefulWidget> {
-  TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
 
   @override
-  Widget build(BuildContext context) {
-   
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'mwangi.inc',
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Sign in',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
+ Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Form(
+                child: Theme(
+                  data: ThemeData(
+                      brightness: Brightness.dark,
+                      primarySwatch: Colors.teal,
+                      inputDecorationTheme: InputDecorationTheme(
+                          labelStyle:
+                              TextStyle(color: Colors.teal, fontSize: 20.0))),
+                  child: new Container(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                              decoration: InputDecoration(
+                                labelText: "enter username",
+                              ),
+                              keyboardType: TextInputType.text),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "enter password",
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          new Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                          ),
+                          new MaterialButton(
+                            onPressed: () => {},
+                            splashColor: Colors.redAccent,
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            child: new Text("Login"),
+                          ),
+                           TextButton(
+                            child: const Text(
+                              'Sign in',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const SecondRoute()),
+                              );
+                            },
+                          )
+                        ],
+                      )),
                 ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                //forgot password screen
-              },
-              child: const Text('Forgot Password',),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Login'),
-                  onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
-                )
-            ),
-            Row(
-              children: <Widget>[
-                const Text('Does not have account?'),
-                TextButton(
-                  child: const Text(
-                    'Sign in',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                   Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondRoute()),);
-                  },
-                )
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ],
-        ));
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
 
@@ -123,12 +113,6 @@ class SecondRoute extends StatelessWidget {
       body: Stack(
         fit: StackFit.expand,
         children: <Widget>[
-          Image(
-            image: AssetImage('assets/look.jpg'),
-            fit: BoxFit.cover,
-            color: Colors.black87,
-            colorBlendMode: BlendMode.darken,
-          ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.center,
@@ -174,10 +158,21 @@ class SecondRoute extends StatelessWidget {
                             textColor: Colors.white,
                             child: new Text("register"),
                           ),
+                           TextButton(
+                            child: const Text(
+                              'log in',
+                              style: TextStyle(fontSize: 20),
+                            ),
+                            onPressed: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => const MyStatefulWidget()),
+                              );
+                            },
+                          )
                         ],
-                        
                       )),
-                      
                 ),
               )
             ],
