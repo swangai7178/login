c2d2918eae85874cdada9c732a38646cc6375f1e
samuel wangai <44896330+swangai7178@users.noreply.github.com>
2022-03-02 09:51:56 +0300

    new c

--- login_php/lib/main.dart
+++ login_php/lib/main.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print, unnecessary_new, prefer_const_constructors
+
 import 'package:flutter/material.dart';
  import 'si.dart';
 void main() => runApp(MyApp());
@@ -100,7 +102,7 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
                     style: TextStyle(fontSize: 20),
                   ),
                   onPressed: () {
-                   Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRoute()),);
+                   Navigator.push(context, MaterialPageRoute(builder: (context) => const SecondRoute()),);
                   },
                 )
               ],
@@ -111,106 +113,77 @@ class _MyStatefulWidgetState extends State<MyStatefulWidget> {
   }
 }
 
-class SecondRoute extends StatefulWidget{
-  const SecondRoute({
-    Key? key,
-    this.color = const Color(0xFFFFE306),
-    this.child,
-  }) : super(key: key);
-
-  final Color color;
-  final Widget? child;
-
-  @override
-  State<SecondRoute> createState() => _SecondRoute();
-    }
-class _SecondRoute extends State<SecondRoute> {
-
-TextEditingController nameController = TextEditingController();
-  TextEditingController passwordController = TextEditingController();
+class SecondRoute extends StatelessWidget {
+  const SecondRoute({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    //MaterialApp(
-    //  debugShowCheckedModeBanner: false
-    // );
-
-   
-    return Padding(
-        padding: const EdgeInsets.all(10),
-        child: ListView(
-          children: <Widget>[
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'mwangi.inc',
-                  style: TextStyle(
-                      color: Colors.blue,
-                      fontWeight: FontWeight.w500,
-                      fontSize: 30),
-                )),
-            Container(
-                alignment: Alignment.center,
-                padding: const EdgeInsets.all(10),
-                child: const Text(
-                  'Register',
-                  style: TextStyle(fontSize: 20),
-                )),
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: TextField(
-                controller: nameController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'User Name',
+    return Scaffold(
+      backgroundColor: Colors.greenAccent,
+      body: Stack(
+        fit: StackFit.expand,
+        children: <Widget>[
+          Image(
+            image: AssetImage('assets/look.jpg'),
+            fit: BoxFit.cover,
+            color: Colors.black87,
+            colorBlendMode: BlendMode.darken,
+          ),
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Form(
+                child: Theme(
+                  data: ThemeData(
+                      brightness: Brightness.dark,
+                      primarySwatch: Colors.teal,
+                      inputDecorationTheme: InputDecorationTheme(
+                          labelStyle:
+                              TextStyle(color: Colors.teal, fontSize: 20.0))),
+                  child: new Container(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Column(
+                        children: <Widget>[
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "enter email",
+                            ),
+                            keyboardType: TextInputType.emailAddress,
+                          ),
+                           TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "enter username",
+                            ),
+                            keyboardType: TextInputType.text
+                          ),
+                          TextFormField(
+                            decoration: InputDecoration(
+                              labelText: "enter password",
+                            ),
+                            keyboardType: TextInputType.text,
+                            obscureText: true,
+                          ),
+                          new Padding(
+                            padding: const EdgeInsets.only(top: 20.0),
+                          ),
+                          new MaterialButton(
+                            onPressed: () => {},
+                            splashColor: Colors.redAccent,
+                            color: Colors.blue,
+                            textColor: Colors.white,
+                            child: new Text("register"),
+                          ),
+                        ],
+                        
+                      )),
+                      
                 ),
-              ),
-            ),
-            Container(
-              padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
-              child: TextField(
-                obscureText: true,
-                controller: passwordController,
-                decoration: const InputDecoration(
-                  border: OutlineInputBorder(),
-                  labelText: 'Password',
-                ),
-              ),
-            ),
-            TextButton(
-              onPressed: () {
-                //forgot password screen
-              },
-              child: const Text('Forgot Password',),
-            ),
-            Container(
-                height: 50,
-                padding: const EdgeInsets.fromLTRB(10, 0, 10, 0),
-                child: ElevatedButton(
-                  child: const Text('Login'),
-                  onPressed: () {
-                    print(nameController.text);
-                    print(passwordController.text);
-                  },
-                )
-            ),
-            Row(
-              children: <Widget>[
-                const Text('Does not have account?'),
-                TextButton(
-                  child: const Text(
-                    'Sign in',
-                    style: TextStyle(fontSize: 20),
-                  ),
-                  onPressed: () {
-                   Navigator.push(context, MaterialPageRoute(builder: (context) => SecondRoute()),);
-                  },
-                )
-              ],
-              mainAxisAlignment: MainAxisAlignment.center,
-            ),
-          ],
-        ));
+              )
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
